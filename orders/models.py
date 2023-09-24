b3d057e2a92ef7c83d96bcd5e5f70cf2d2e3c074
amr <amr.elsaeed995@gmail.com>
2023-09-24 23:55:40 +0300

    Initial commit

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,11 @@
+from decimal import Decimal
+from django.core.validators import MinValueValidator, \
+                                   MaxValueValidator
 from django.db import models
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
+
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -13,19 +18,37 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                       MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
         indexes = [
-        models.Index(fields=['-created']),
+            models.Index(fields=['-created']),
         ]
 
     def __str__(self):
-        return f'Order {self.id}'  #The f string formatting operator is used to format the string with the value of the id attribute
-    
-    def get_total_cost(self):
+        return f'Order {self.id}'
+
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
-    
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
     def get_stripe_url(self):
         if not self.stripe_id:
             # no payment associated
@@ -37,8 +60,6 @@ class Order(models.Model):
             # Stripe path for real payments
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
-    
-    stripe_id = models.CharField(max_length=250, blank=True)
 
 
 class OrderItem(models.Model):
@@ -54,9 +75,9 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.id)
+
     def get_cost(self):
         return self.price * self.quantity
-
  
     
  
